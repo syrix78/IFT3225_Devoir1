b70007ee26deaa847af9badc0bd60bd8498b2ede
syrix78 <lucas.hornung@hotmail.com>
2020-03-10 01:01:04 -0400

    Piano is now working on tablets and phones

--- piano.js
+++ piano.js
@@ -17,8 +17,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
       audio.currentTime = 0;
       audio.play();
     }
+    
+    function playNoteOnClick(e) {
+    
+      const audio = document.querySelector(`audio[data-key="${e}"]`),
+        key = document.querySelector(`.key[data-key="${e}"]`);
+
+      if (!key) return;
+
+      const keyNote = key.getAttribute("data-note");
+
+      key.classList.add("playing");
+      note.innerHTML = keyNote;
+      audio.currentTime = 0;
+      audio.play();
+    }
 
     function removeTransition(e) {
+        console.log(e.propertyName);
       if (e.propertyName !== "transform") return;
       this.classList.remove("playing");
     }
@@ -26,10 +42,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
     function hintsOn(e, index) {
       e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
     }
+    
 
     hints.forEach(hintsOn);
 
     keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+    keys.forEach(key => key.addEventListener("click", function (event) {
+        var keyPressed = event.target.innerText;
+
+        if(keyPressed == ';'){
+            playNoteOnClick(186)
+        }
+        else{
+            keyPressed = keyPressed.charCodeAt(0);
+            playNoteOnClick(keyPressed)
+        } 
+        }));
 
     window.addEventListener("keydown", playNote);
+    
+    
 });
